43e94f6dfc8f59d7e1f92e6fb365da5e2f7e0b64
Hamit Mizrak <hamitmizrak@gmail.com>
2020-08-06 23:35:27 +0300

    JSF ,  PrimeFaces , Hibernate , Web Servis ayarları yapıldı

---
+++ src/main/java/com/java/Tutorials_008_JSF_Primefaces/App.java
@@ -0,0 +1,13 @@
+package com.java.Tutorials_008_JSF_Primefaces;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
